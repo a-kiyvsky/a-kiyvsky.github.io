dc7dc8507f02498228f9c3b95025b7e6896e251f
Alexey Kiyvsky <crytekunitytek@gmail.com>
2017-10-16 07:24:37 +0300

    NCrawler Updates

--- ncrawler/js/common.js
+++ ncrawler/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
 
-	// Custom JS
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 60) {
+        //clearHeader, not clearheader - caps H
+        $('header').addClass('scroll');
+    } else {
+      if(scroll <60) {
+        $('header').removeClass('scroll')
+      }
+    }
+}); //missing );
+	$('#nav-icon1').click(function(){
+		$('nav').toggleClass('open');
+    $(this).toggleClass('open');
+	});
 
 });
