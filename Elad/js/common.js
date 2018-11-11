6e9d9f07c2b62646d267f531c6e2d32c3d486928
Alexey Lobyak <crytekunitytek@gmail.com>
2018-11-12 00:09:40 +0300

    Added Elad Website

---
+++ Elad/js/common.js
@@ -0,0 +1,36 @@
+$(function() {
+	$('.sigContain').each(function(index) {
+		var contain = $(this);
+		setTimeout(function() {
+			contain.addClass('active');
+			console.log($(this));
+		}, 750 * index);
+	})
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:false,
+		responsive:{
+			0:{
+				items:1
+			},
+		}
+	})
+
+	var wave1 = $('#wawe1').wavify({
+		height: 380,
+		bones: 4,
+		amplitude: 60,
+		color: '#fff',
+		speed: .15
+	});
+
+  var wave2 = $('#wawe2').wavify({
+		height: 380,
+		bones: 3,
+		amplitude: 40,
+		color: '#fce98b',
+		speed: .25
+  });
+
+});
