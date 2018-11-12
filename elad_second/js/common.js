e034308577b1d41fad1fdd2649571084626b758f
Alexey Lobyak <crytekunitytek@gmail.com>
2018-11-12 19:27:32 +0300

    updates

--- elad_second/js/common.js
+++ elad_second/js/common.js
@@ -16,20 +16,20 @@ $(function() {
 		}
 	})
 
-	var wave1 = $('#wawe1').wavify({
-		height: 380,
-		bones: 4,
-		amplitude: 60,
-		color: '#fff',
-		speed: .15
-	});
-
-  var wave2 = $('#wawe2').wavify({
-		height: 380,
-		bones: 3,
-		amplitude: 40,
-		color: '#fce98b',
-		speed: .25
-  });
+	// var wave1 = $('#wawe1').wavify({
+	// 	height: 380,
+	// 	bones: 4,
+	// 	amplitude: 60,
+	// 	color: '#fff',
+	// 	speed: .15
+	// });
+	//
+  // var wave2 = $('#wawe2').wavify({
+	// 	height: 380,
+	// 	bones: 3,
+	// 	amplitude: 40,
+	// 	color: '#fce98b',
+	// 	speed: .25
+  // });
 
 });
